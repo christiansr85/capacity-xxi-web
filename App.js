cebd8a1e3c400efb765442dce3813c578824a983
Cris Sánchez Ruiz <christiansr85@gmail.com>
2020-05-24 19:34:14 +0200

    Adds first files to web.

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+// import SearchView from './pages/SearcherView';
+
+function App() {
+    return (
+        <Fragment>
+            {/* <Switch>
+                <Route exact path="/">
+                    <SearchView />
+                </Route>
+                
+                <Route path="/search">
+                    <SearchView />
+                </Route>
+            </Switch> */}
+        </Fragment>
+    )
+}
+
+export default App;
